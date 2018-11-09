5c2b763a572ff6a11284f5e06085399dcb8bc169
djiang2 <djiang2>
2018-11-09 10:38:52 -0500

    init2

--- main.c
+++ main.c
@@ -7,12 +7,14 @@ static void sighandler(int signo){
   if(SIGINT == signo){
     printf("\n Program exited due to SIGINT \n");
     exit(0);
+  }else if(SIGUSR1 == signo){
+    printf("\n Parent Process: %d \n", getppid());
   }
-
 }
 int main(){
 
   signal(SIGINT,sighandler);
+  signal(SIGUSR1, sighandler);
 
   while(1){
     printf("Process: %d \n",getpid());
